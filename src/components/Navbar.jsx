40ece1736237cdf2ceff69d032f6fbda81fbae76
iabdulhakim <abdulhakimisroil@gmail.com>
2024-06-27 17:59:41 +0500

    last commits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,18 @@ import Link from 'next/link';
 function Navbar() {
   return (
     <div>
-        <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100">
         <div className="flex-1">
           <a href="./" className="btn btn-ghost text-xl">Charts</a>
         </div>
-        
+
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
               <Link href={'/piechart'}>Classwork</Link>
             </li>
             <li>
-              <Link href={'/barchart'}>Population</Link>
+              <Link href={'/barchart'}>Populations</Link>
             </li>
             <li>
               <Link href={'/barchart2'}>Area</Link>
